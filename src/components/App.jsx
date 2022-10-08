679e4b3ab4cf36170dd552023ef19bf3986effd1
Kseniia30 <kellyshikova3004@gmail.com>
2022-10-08 10:26:25 +0200

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,14 @@ import user from "./Profile/user.json"
 import data from "./Statistics/data.json"
 import friends from "./Friends/friends.json"
 import transactions from "./Transaction/transactions.json"
+import css from "./App.module.css"
 
 
 
 export const App = () => {
   return (
-    <div
-      style={{
-        width: 1200,
-        padding: 15,
-        marginLeft: "auto",
-        marginRight: "auto"
-    }}>
-      <h1
-      style={{
-        textAlign: 'center',
-        fontSize: 40,
-        color: 'red'
-      }}>TASK 1</h1>
+    <div className={css.container}>
+      <h1 className={css.topText}>TASK 1</h1>
       <Profile
         username={user.username}
         tag={user.tag}
@@ -32,48 +22,18 @@ export const App = () => {
         stats={user.stats} />
       
 
-      <h1
-      style={{
-        textAlign: 'center',
-        fontSize: 40,
-        color: 'red',
-        marginTop: 20,
-        }}>TASK 2</h1>
-      <h2
-      style={{
-        textAlign: 'center',
-        fontSize: 20,
-        color: 'green',
-        marginBottom: 10,
-      }}>part 1</h2>
+      <h1 className={css.topText}>TASK 2</h1>
+      <h2 className={css.headerText}>part 1</h2>
       <Statistics title="Upload stats" stats={data} />
-      <h2
-      style={{
-        textAlign: 'center',
-        fontSize: 20,
-        color: 'green',
-        marginBottom: 10,
-      }}>part2</h2>
+      <h2 className={css.headerText}>part2</h2>
       <Statistics stats={data} />
 
 
-      <h1
-      style={{
-        textAlign: 'center',
-        fontSize: 40,
-        color: 'red',
-        marginTop: 20,
-      }}>TASK 3</h1>
+      <h1 className={css.topText}>TASK 3</h1>
       <FriendList friends={friends} />
 
 
-      <h1
-      style={{
-        textAlign: 'center',
-        fontSize: 40,
-        color: 'red',
-        marginTop: 20,
-      }}>TASK 4</h1>
+      <h1 className={css.topText}>TASK 4</h1>
       <TransactionHistory items={transactions} />
     </div>
   );
--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -4,17 +4,21 @@ import css from "./Friends.module.css";
 export const FriendList = ({ friends }) => {
     return (
         <ul className={css.friendList}>
-            {friends.map(({id, isOnline, avatar, name}) => (
-                <li className={css.friendsItem} key={id}>
-                    <span className={css.status} style={{backgroundColor: isOnline ? "green" : "red" }}>{isOnline}</span>
-                    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-                    <p className={css.friendsName}>{ name}</p>
-                </li>
+            {friends.map(friend => (
+                <FriendListItem key={friend.id} friend={ friend} />
             ))}
         </ul>
     )
 }
-
+const FriendListItem = ({friend: { id, isOnline, avatar, name }}) => {
+    return (
+        <li className={css.friendsItem} key={id}>
+            <span className={css.status} style={{backgroundColor: isOnline ? "green" : "red" }}>{isOnline}</span>
+            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <p className={css.friendsName}>{ name}</p>
+        </li>
+    )
+}
 FriendList.protoTypes = {
     friends: PropTypes.arrayOf(
         PropTypes.exact({
